22d7a780ccac3a157885a74aa5081a83ef7bf279
Jonathan Hammond <hamman359@gmail.com>
2018-12-27 18:02:34 -0500

    Initial Commit. Setting up solution

---
+++ MazeGenerator.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MazeGenerator.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
