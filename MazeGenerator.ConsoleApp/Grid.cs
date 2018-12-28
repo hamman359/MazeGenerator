04cf182d6c186419e1ab21857ffd4c388eb8e2ee
Jonathan Hammond <hamman359@gmail.com>
2018-12-27 21:32:52 -0500

    Adding Basic Grid Implementation.

---
+++ MazeGenerator.ConsoleApp/Grid.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MazeGenerator.ConsoleApp
+{
+    public class Grid
+    {
+        public int Columns { get; }
+        public int Rows { get; }
+        public int Size => Rows * Columns;
+
+        readonly Cell[][] _grid;
+
+        public Grid(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+
+            _grid = new Cell[Rows][];
+
+            PrepareGrid();
+            ConfigureCells();
+        }
+
+        protected void PrepareGrid()
+        {
+            for (int row = 0; row < Rows; row++)
+            {
+                _grid[row] = new Cell[Columns];
+
+                for (int col = 0; col < Columns; col++)
+                {
+                    _grid[row][col] = new Cell(row, col);
+                }
+            }
+        }
+
+        protected void ConfigureCells()
+        {
+            for (int row = 0; row < Rows; row++)
+            {
+                for (int col = 0; col < Columns; col++)
+                {
+                    var cell = _grid[row][col];
+
+                    SetNorth(cell, row, col);
+                    SetSouth(cell, row, col);
+                    SetEast(cell, row, col);
+                    SetWest(cell, row, col);
+                }
+            }
+        }
+
+        void SetNorth(Cell cell, int row, int col)
+        {
+            if (row > 0)
+            {
+                cell.North = _grid[row - 1][col];
+            }
+            else
+            {
+                cell.North = null;
+            }
+        }
+
+        void SetSouth(Cell cell, int row, int col)
+        {
+            if (row < Rows - 1)
+            {
+                cell.South = _grid[row + 1][col];
+            }
+            else
+            {
+                cell.South = null;
+            }
+        }
+
+        void SetEast(Cell cell, int row, int col)
+        {
+            if (col < Columns - 1)
+            {
+                cell.East = _grid[row][col + 1];
+            }
+            else
+            {
+                cell.East = null;
+            }
+        }
+
+        void SetWest(Cell cell, int row, int col)
+        {
+            if (col > 0)
+            {
+                cell.West = _grid[row][col - 1];
+            }
+            else
+            {
+                cell.West = null;
+            }
+        }
+
+        public Cell GetRandomCell()
+        {
+            Random random = new Random(DateTime.Now.Millisecond);
+
+            var row = random.Next(Rows);
+            var col = random.Next(Columns);
+
+            return _grid[row][col];
+        }
+
+        public IEnumerable<Cell[]> IterateRows()
+        {
+            for (int row = 0; row < Rows; row++)
+            {
+                yield return _grid[row];
+            }
+        }
+
+        public IEnumerable<Cell> IterateEachCell()
+        {
+            for (int row = 0; row < Rows; row++)
+            {
+                for (int col = 0; col < Columns; col++)
+                {
+                    yield return _grid[row][col];
+                }
+            }
+        }
+    }
+}
--- MazeGenerator.ConsoleApp/Program.cs
+++ MazeGenerator.ConsoleApp/Program.cs
@@ -8,7 +8,33 @@ namespace MazeGenerator.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var grid = new Grid(5,5);
+
+            //while (cell.Column != 4)
+            //{
+
+            //    cell = grid.GetRandomCell();
+            //}
+            //for (int i = 0; i < 200; i++)
+            //{
+
+            //    Cell cell = grid.GetRandomCell();
+            //    Console.WriteLine($"Row: {cell.Row}, Column: {cell.Column}");
+            //}
+
+            //var rows = grid.IterateRows();
+
+            //foreach (var row in rows)
+            //{
+            //    Console.WriteLine($"Row: {row.Length}");
+            //}
+
+            var cells = grid.IterateEachCell();
+
+            foreach (var cell in cells)
+            {
+                Console.WriteLine($"Row: {cell.Row}, Col: {cell.Column}");
+            }
         }
     }
 }
